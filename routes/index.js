58c3790d65d73aa6a28e13e0d1411ccc15df8282
Dora Do <dora.thien.do@gmail.com>
2022-10-25 20:57:46 -0400

    add GET and POST requests

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var neDB = require('nedb');
+var db = new neDB({ filename: 'my.db', autoload: true });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/all', function(req, res){
+  db.find({}, function (err, docs) {
+    if (err) {
+      res.sendStatus(400).send(err);
+    } else {
+      res.send(docs);
+    }
+  });
+});
+
+router.post('/add', function (req, res) {
+  var data = [ { name: 'John', score: '48' },
+    { name: 'Henry', score: '19' },
+    { name: 'Albert', score: '15' } ];
+  db.insert(data, function (err, newDocs) {
+    if (err) {
+      res.sendStatus(400).send(err);
+    } else {
+      res.send('Success!');
+    }
+  });
 });
 
 module.exports = router;
